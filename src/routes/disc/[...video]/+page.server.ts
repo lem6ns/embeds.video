b05526c19f1f271f0f64194d23ca7cba2302bd3e
cloud <62519659+lem6ns@users.noreply.github.com>
2022-12-16 13:56:17 -0700

    Hotfix

--- src/routes/disc/[...video]/+page.server.ts
+++ src/routes/disc/[...video]/+page.server.ts
@@ -4,6 +4,6 @@ export const load = (async ({ params }) => {
     const attachments = !params.video.startsWith("attachments/") ? "attachments/" : ""
 
     return {
-        url: `http://embeds.video/discprox/${attachments}${params.video}`
+        url: `http://embeds.video/discproxy/${attachments}${params.video}`
     }
 }) satisfies PageServerLoad;
