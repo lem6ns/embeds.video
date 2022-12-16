e8e74f42b5cd449c9105bc9ec1c506b378559370
cloud <62519659+lem6ns@users.noreply.github.com>
2022-12-16 14:30:49 -0700

    Use nfp.is discord proxy

--- src/routes/disc/[...video]/+page.server.ts
+++ src/routes/disc/[...video]/+page.server.ts
@@ -4,6 +4,6 @@ export const load = (async ({ params }) => {
     const attachments = !params.video.startsWith("attachments/") ? "attachments/" : ""
 
     return {
-        url: `http://embeds.video/discproxy/${attachments}${params.video}`
+        url: `https://discordproxy.nfp.is/${attachments}${params.video}`
     }
 }) satisfies PageServerLoad;
